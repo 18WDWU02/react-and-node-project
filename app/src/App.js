a5c56c24ce399290245e4bac58ddafed4d4f6ad7
RichardHpaYoobee <richard.hpa@acgedu.com>
2018-09-04 09:44:46 +1200

    added files from both server and app

---
+++ app/src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://192.168.33.10:5000/getjson")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.first_name}>
+              {item.first_name} {item.last_name}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
+
+export default App;
